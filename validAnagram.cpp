689eb5a1275a19265f90a7aa95e4ce0bb0e78815
natan kassa <nkassa@usc.edu>
2025-02-08 16:04:22 -0800

    new problem

--- validAnagram.cpp
+++ validAnagram.cpp
@@ -3,16 +3,26 @@ public:
     bool isAnagram(string s, string t) 
     {
         unordered_map<char,int> count;
-        unordered_map<char,int> count_two;
-        for(auto letter: t)
+        for(auto letter: s)
         {
             count[letter]++;
         }
-        for(auto letter: s)
-        {
-            count_two[letter]++;
-        }
 
-        return count == count_two;
+        for(auto letter: t)
+        {
+            if(count.find(letter) != count.end())
+            {
+                count[letter]--;
+                if(count[letter] == 0)
+                {
+                    count.erase(letter);
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return count.size() == 0;
     }
 };
