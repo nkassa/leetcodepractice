e0e7ce1483912f76a532f66be4cd22f35435681e
natan kassa <nkassa@usc.edu>
2025-02-18 20:44:30 -0800

    new problem

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,18 +1,19 @@
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
         unordered_map<int,int> count;
-        for(int i = 0; i < nums.size(); i++)
+        for(int i = 0; i < nums.size();i++)
         {
-            if(count.find(target - nums[i]) != count.end())
+            int diff = target - nums[i];
+            if(count.find(diff) != count.end())
             {
-                return {i, count[target - nums[i]]};
+                return {i, count[diff]};
             }
             count[nums[i]] = i;
         }
         return {};
     }
 };
-
-//hi
