e86db9415ec3eca0d072ac7a228c199968b48fde
natan kassa <nkassa@usc.edu>
2025-02-21 14:48:12 -0800

    new problem

--- validPalindrome.cpp
+++ validPalindrome.cpp
@@ -6,25 +6,22 @@ public:
         int right = s.size()-1;
         while(left < right)
         {
-            if(isalnum(s[left]) == false)
+            if(!isalnum(s[left]))
             {
                 left++;
             }
-            if(isalnum(s[right]) == false)
+            if(!isalnum(s[right]))
             {
                 right--;
             }
             if(isalnum(s[left]) && isalnum(s[right]))
             {
-                if(tolower(s[right]) == tolower(s[left]))
-                {
-                    left++;
-                    right--;
-                }
-                else
+                if(tolower(s[right]) != tolower(s[left]))
                 {
                     return false;
                 }
+                left++;
+                right--;
             }
         }
         return true;
