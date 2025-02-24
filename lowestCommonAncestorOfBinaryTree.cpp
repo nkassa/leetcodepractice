4a97c9975684eda9b640fcac29fd5d0220eae25f
natan kassa <nkassa@usc.edu>
2025-02-23 17:38:41 -0800

    new problem

--- lowestCommonAncestorOfBinaryTree.cpp
+++ lowestCommonAncestorOfBinaryTree.cpp
@@ -12,18 +12,11 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
     {
-        if(root == nullptr)
-        {
-            return nullptr;
-        }
-        if((root->val > p->val && root->val < q->val) || (root->val < p->val && root->val > q->val))
+        if(root == nullptr || root->val == p->val || root->val == q->val || (root->val > p->val && root->val < q->val) || (root->val < p->val && root->val > q->val))
         {
             return root;
         }
-        if(root->val == p->val || root->val == q->val)
-        {
-            return root;
-        }
+
         if(root->val > p->val && root->val > q->val)
         {
             return lowestCommonAncestor(root->left, p, q); 
