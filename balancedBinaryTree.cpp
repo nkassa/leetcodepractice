0c156ee3d807ef272ad8667edb1822510499b62b
natan kassa <nkassa@usc.edu>
2025-02-24 21:07:54 -0800

    new problem

---
+++ balancedBinaryTree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool ans = true;
+    bool isBalanced(TreeNode* root) 
+    {
+        if(root == nullptr)
+        {
+            return true;
+        }
+        dfs(root, 0);
+        return ans;
+    }
+    int dfs(TreeNode* root, int cnt)
+    {
+        if(!root->right && !root->left)
+        {
+            return cnt;
+        }
+        int left = dfs(root->left, cnt++);
+        int right = dfs(root->right, cnt++);
+        cout << left << " " << right << endl;
+        if(abs(right-left) > 1)
+        {
+            ans = false;
+        }
+        return cnt;
+    }
+};
