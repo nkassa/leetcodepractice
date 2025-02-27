ffd936113e00a39697d61f88ee13925a38cd2e5c
natan kassa <nkassa@usc.edu>
2025-02-26 16:28:46 -0800

    new problem

---
+++ longestPalindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestPalindrome(string s) 
+    {
+        int cnt = 0;
+        unordered_map<char,int> count;
+        for(char c: s)
+        {
+            count[c]++;
+        }
+        for(auto [key,val]: count)
+        {
+            while(val > 2)
+            {
+                cnt++;
+                count[key] -= 2;
+                if(val == 0)
+                {
+                    count.erase(key);
+                }
+            }
+        }
+        if(count.size() == 0)
+        {
+            return cnt;
+        }
+        return cnt;
+    }
+};
