ac9ffd21c0960b3126ec78a3b49afd1af7d14564
natan kassa <nkassa@usc.edu>
2025-03-02 23:58:46 -0800

    new problem

---
+++ containsDuplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        unordered_map<int,int> count;
+        for(int num: nums)
+        {
+            count[num]++;
+            if(count[num] > 1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
