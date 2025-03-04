03eb3c3f53ea3e5ac78f8294ebd582c8208dc9f2
natan kassa <nkassa@usc.edu>
2025-03-03 20:57:53 -0800

    new problem

--- longestSubstringWithoutRepeatingCharacters.cpp
+++ longestSubstringWithoutRepeatingCharacters.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) 
     {
-        unordered_map<char,int> count;
+        unordered_map<char, int> count;
         int left = 0;
         int ans = 0;
         for(int right = 0; right < s.size(); right++)
         {
-            count[s[right]] += 1;
+            count[s[right]]++;
             while(count[s[right]] > 1)
             {
                 count[s[left]]--;
