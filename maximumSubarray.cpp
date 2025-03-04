0902107ded21ab9b55bcacbb915b07daabff7296
natan kassa <nkassa@usc.edu>
2025-03-03 17:23:00 -0800

    new problem

--- maximumSubarray.cpp
+++ maximumSubarray.cpp
@@ -2,11 +2,6 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) 
     {
-        if(nums.size() == 0)
-        {
-            return 0;
-        }
-        int left = 0;
         int sum = 0;
         int ans = INT_MIN;
         for(int right = 0; right < nums.size(); right++)
@@ -16,7 +11,6 @@ public:
             if(sum < 0)
             {
                 sum = 0;
-                left++;
             }
         }
         return ans;
