091e42fb2a31b381f469b19423fd2f8ba7d03629
natan kassa <nkassa@usc.edu>
2025-03-03 20:47:11 -0800

    new problem

--- kClosestPointsToOrigin.cpp
+++ kClosestPointsToOrigin.cpp
@@ -6,16 +6,14 @@ public:
         priority_queue<vector<int>> heap;
         for(vector<int> point: points)
         {
-            int dist = sqrt(pow((point[0]-0),2)+ pow((point[1]-0), 2));
+            int dist = pow((point[0]-0),2)+ pow((point[1]-0), 2);
             heap.push({dist, point[0], point[1]});
+            if(heap.size() > k)
+            {
+                heap.pop();
+            }
         }
-        int i = 0;
-        while(i < points.size()-k)
-        {
-            heap.pop();
-            i++;
-        }
-        for(int i = 0; i < k; i++)
+        while(!heap.empty())
         {
             ans.push_back({heap.top()[1], heap.top()[2]});
             heap.pop();
