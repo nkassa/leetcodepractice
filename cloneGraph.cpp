6df050a69b7ec3e8d8bc5a56f42d8e4f8f4f9288
natan kassa <nkassa@usc.edu>
2025-03-04 17:14:38 -0800

    new problem

--- cloneGraph.cpp
+++ cloneGraph.cpp
@@ -35,14 +35,14 @@ public:
         }
         return dfs(node, count);
     }
-    Node* dfs(Node* node, unordered_map<Node*,Node*> count)
+    Node* dfs(Node* node, unordered_map<Node*,Node*>& count)
     {
         vector<Node*> neighbor;
         Node* clone = new Node(node->val);
         count[node] = clone;
         for(Node* newNode: node->neighbors)
         {
-            if(count[newNode] != count.end())
+            if(count.find(newNode) != count.end())
             {
                 neighbor.push_back(count[newNode]);
             }
@@ -51,7 +51,7 @@ public:
                 neighbor.push_back(dfs(newNode, count));
             }
         }
-        clone->neighbors = neighbour;
+        clone->neighbors = neighbor;
         return clone;
     }
 };
