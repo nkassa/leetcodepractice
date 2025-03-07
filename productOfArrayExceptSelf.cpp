fbd0f829052eef75ea4404f26d9f41ead56dfbb3
natan kassa <nkassa@usc.edu>
2025-03-06 21:11:08 -0800

    new problem

--- productOfArrayExceptSelf.cpp
+++ productOfArrayExceptSelf.cpp
@@ -5,13 +5,13 @@ public:
         vector<int> prefix(nums.size());
         vector<int> suffix(nums.size());
         prefix[0] = nums[0];
-        suffix[nums.size()-1] = nums[];
+        suffix[nums.size()-1] = nums[nums.size()-1];
         int left = 1;
         int right = nums.size()-2;
         while(left < nums.size())
         {
-            prefix.push_back(prefix.back() * nums[left]);
-            suffix.push_back(suffix.back() * nums[right]);
+            prefix[left] = prefix[left-1] * nums[left];
+            suffix[right] = suffix[right+1] * nums[right]);
             left++;
             right--;
         }
