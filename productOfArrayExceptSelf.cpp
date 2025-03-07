d0f47e2c422915a5deca7c2c353922db8ca1bac4
natan kassa <nkassa@usc.edu>
2025-03-06 21:56:22 -0800

    new problem

--- productOfArrayExceptSelf.cpp
+++ productOfArrayExceptSelf.cpp
@@ -4,30 +4,32 @@ public:
     {
         vector<int> prefix(nums.size());
         vector<int> suffix(nums.size());
+
         prefix[0] = nums[0];
         suffix[nums.size()-1] = nums[nums.size()-1];
+
         int left = 1;
         int right = nums.size()-2;
+
         while(left < nums.size())
         {
             prefix[left] = prefix[left-1] * nums[left];
             suffix[right] = suffix[right+1] * nums[right];
+
             left++;
             right--;
         }
+
         vector<int> ans(nums.size());
         ans[0] = suffix[1];
         ans[nums.size()-1] = prefix[nums.size() - 2];
+
         for(int i = 1; i < nums.size()-1; i++)
         {
-            cout << prefix[i-1] << " " <<  suffix[i+1] << endl;
             ans[i] = prefix[i-1] * suffix[i+1];
         }
+        
         return ans;
     }
 };
 
-
-
-// 1   2   6   24
-// 24  24  12  4
