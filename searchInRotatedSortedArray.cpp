fa134ce92e625bcb25de50aa3066b2804b20993f
natan kassa <nkassa@usc.edu>
2025-03-07 23:45:44 -0800

    new problem

--- searchInRotatedSortedArray.cpp
+++ searchInRotatedSortedArray.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int left = 0; 
+    int search(vector<int>& nums, int target) 
+    {
+        int left = 0;
         int right = nums.size()-1;
         while(left <= right)
         {
-            int mid = left + ((right-left)/2);
+            int mid = left + (right-left)/2;
             if(nums[mid] == target)
             {
                 return mid;
             }
-            else if(nums[mid] >= nums[left])
+            else if(nums[left] < target && nums[mid] > target)
             {
-                if(nums[mid] >= target && nums[left] <= target )
-                {
-                    right = mid - 1;
-                }
-                else
-                {
-                    left = mid + 1;
-                }
+                right = mid-1;
             }
             else
             {
-                if(nums[mid] <= target && nums[right] >= target )
-                {
-                    left = mid + 1;
-                }
-                else
-                {
-                    right = mid - 1;
-                }
+                left = mid+1;
             }
         }
         return -1;
