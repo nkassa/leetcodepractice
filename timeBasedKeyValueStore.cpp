82157e39878effd065014778bb8ce8cd5ee4c76b
natan kassa <nkassa@usc.edu>
2025-03-17 23:47:59 -0700

    new problem

--- timeBasedKeyValueStore.cpp
+++ timeBasedKeyValueStore.cpp
@@ -1,33 +1,17 @@
 class TimeMap {
 public:
-    unordered_map<string, vector<pair<int, string>>> store;
+    unorered_map<string, vector<pair<int,string>>> count;
     TimeMap() {  
     }
     
     void set(string key, string value, int timestamp) {
-        store[key].push_back(pair(timestamp,value));
+        count[key].push_back({timestanp, value}));
     }
     
     string get(string key, int timestamp) 
     {
-        vector<pair<int, string>>& numStr = store[key];
-        string ans;
-        int left = 0;
-        int right = numStr.size()-1;
-        while(left <= right)
-        {
-            int mid = left + ((right-left)/2);
-            if(numStr[mid].first <= timestamp)
-            {
-                ans = numStr[mid].second;
-                left = mid + 1;
-            }
-            else
-            {
-                right = mid - 1;
-            }
-        }
-        return ans;
+        vector<pair<int,string>> list;
+        list = count[key];
     }
 };
 
