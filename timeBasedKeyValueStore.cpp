b3084e966680c1f6e7a8b746d87ff74ec885111b
natan kassa <nkassa@usc.edu>
2025-03-17 23:51:11 -0700

    new problem

--- timeBasedKeyValueStore.cpp
+++ timeBasedKeyValueStore.cpp
@@ -12,6 +12,21 @@ public:
     {
         vector<pair<int,string>> list;
         list = count[key];
+        int left = 0; 
+        int right = list.size()-1;
+        while(left <= right)
+        {
+            int mid = left + (right-left)/2;
+            if(list[mid].second <= timestamp)
+            [
+                left = mid + 1;
+            ]
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return count[key]
     }
 };
 
