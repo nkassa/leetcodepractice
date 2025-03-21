b3fca3ea099ea1d01b6a190c842f45e74a8e337c
natan kassa <nkassa@usc.edu>
2025-03-20 23:29:25 -0700

    new problem

--- findAllAnagramsInAString.cpp
+++ findAllAnagramsInAString.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> findAnagrams(string s, string p) 
     {
         unordered_set<char> count;
+        unordered_set<char> list;
         for(int i = 0; i < p.size(); i++)
         {
             count.insert(p[i]);
@@ -11,11 +12,20 @@ public:
         vector<int> ans;
         for(int right = 0; right < s.size(); right++)
         {
-            if(right - left == p.size())
+            if(list.size() == p.size())
             {
                 ans.push_back(left);
+                left = right;
             }
-            if()
+            if(count.find(s[right]) != count.end())
+            {
+                list.insert(s[right]);
+            }
+            else
+            {
+                left = right;
+            }
         }
+        return ans;
     }
 };
