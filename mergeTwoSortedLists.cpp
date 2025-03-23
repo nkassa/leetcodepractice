d036676bd7bc1ab8ca1e48596287071480f32f81
natan kassa <nkassa@usc.edu>
2025-03-22 23:39:08 -0700

    new problem

--- mergeTwoSortedLists.cpp
+++ mergeTwoSortedLists.cpp
@@ -12,31 +12,12 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
     {
-        ListNode* head = new ListNode(0);
-        ListNode* top = head;
-        
+        if(list1 == nullptr)
+        {
+            return list1;
+        }
+        ListNode* ans;
+        if(li)
         while(list1 != nullptr && list2 != nullptr)
-        {
-            if(list1->val <= list2->val)
-            {
-                head->next = list1;
-                list1 = list1->next;
-            }
-            else
-            {
-                head->next = list2;
-                list2 = list2->next;
-            }
-            head = head->next;
-        }
-        if(list1 != nullptr)
-        {
-            head->next = list1;
-        }
-        if(list2 != nullptr)
-        {
-            head->next = list2;
-        }
-        return top->next;
     }
 };
