4ad3d9a23b46ec05729e8f345be5a6156573cbe4
natan kassa <nkassa@usc.edu>
2025-03-27 23:46:31 -0700

    new problem

--- fibonacciNumber.cpp
+++ fibonacciNumber.cpp
@@ -7,12 +7,14 @@ public:
             return n;
         }
         vector<int> list(n+1, -1);
-        list[0] = 0;
-        list[1] = 1;
         return dp(n, list);
     }
     int dp(int n, vector<int>& list)
     {
+        if(n == 0 || n == 1)
+        {
+            return n;
+        }
         if(list[n] >= 0)
         {
             return list[n];
