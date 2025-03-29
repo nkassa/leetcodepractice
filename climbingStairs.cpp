6c1d322c6a9525411ef09de4d9eb1797cb848e9a
natan kassa <nkassa@usc.edu>
2025-03-28 23:47:33 -0700

    new problem

--- climbingStairs.cpp
+++ climbingStairs.cpp
@@ -7,5 +7,13 @@ public:
         steps[0] = 0;
         step[1] = 1;
         step[2] = 2;
+        return dp(n);
+    }
+    int dp(int idx)
+    {
+        if(steps[idx] >= 0)
+        {
+            return steps[idx];
+        }
     }
 };
