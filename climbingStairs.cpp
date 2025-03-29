9188e457f3ba1865bc684ad771d7f6ba98c1989b
natan kassa <nkassa@usc.edu>
2025-03-28 23:50:51 -0700

    new problem

--- climbingStairs.cpp
+++ climbingStairs.cpp
@@ -3,10 +3,14 @@ public:
     vector<int> steps;
     int climbStairs(int n) 
     {
+        if(n <= 2)
+        {
+            return n;
+        }
         steps = vector(n+1, -1);
         steps[0] = 0;
-        step[1] = 1;
-        step[2] = 2;
+        steps[1] = 1;
+        steps[2] = 2;
         return dp(n);
     }
     int dp(int idx)
@@ -15,6 +19,6 @@ public:
         {
             return steps[idx];
         }
-        return steps[idx] = step[idx-2] + step[idx-1];
+        return steps[idx] = steps[idx-2] + steps[idx-1];
     }
 };
