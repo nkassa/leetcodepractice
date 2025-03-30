2190f90e7249afa3fbfe091abe7eb6e1f05facac
natan kassa <nkassa@usc.edu>
2025-03-29 22:25:12 -0700

    new problem

---
+++ houseRobber.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> list;
+    vector<int> nums;
+    int rob(vector<int>& nums) 
+    {
+        this->nums = nums;
+        if(nums.size() == 1)
+        {
+            return nums[0];
+        }
+        list = vector(nums.size(), -1);
+        list[0] = nums[0];
+        list[1] = nums[1];
+        return dp(nums.size());
+    }
+    int dp(int idx)
+    {
+        if(list[idx] != -1)
+        {
+            return list[idx];
+        }
+        list[idx] = max(dp(idx-2)+nums[idx], dp(idx-1));
+        return list[idx];
+    }
+};
