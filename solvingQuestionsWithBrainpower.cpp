a196b5e138f11ebbb6920c12db59fb9568d80a72
natan kassa <nkassa@usc.edu>
2025-03-30 23:52:30 -0700

    new problem

---
+++ solvingQuestionsWithBrainpower.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> memo;
+    vector<vector<int>> questions;
+    int n;
+    long long mostPoints(vector<vector<int>>& questions) 
+    {
+        this->questions = questions;
+        n = questions.size();
+        memo = vector(n, -1);
+        memo[n-1] = questions[n-1][1];
+        return dp(0);
+    }
+
+    long long dp(int idx)
+    {
+        if(idx > n-1)
+        {
+            return 0;
+        }
+        if(memo[idx] != -1)
+        {
+            return memo[idx];
+        }
+        memo[idx] = max(dp(idx+1), idx + questions[idx][1] + 1);
+        return meno[idx];
+    }
+};
