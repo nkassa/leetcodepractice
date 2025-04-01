8496d8e4ac4c848a697a282d30c07e2b89aef27f
natan kassa <nkassa@usc.edu>
2025-03-31 23:47:55 -0700

    new problem

--- minCostClimbingStairs.cpp
+++ minCostClimbingStairs.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    vector<int> list;
+    vector<int> memo;
     vector<int> cost;
     int minCostClimbingStairs(vector<int>& cost) 
     {
         this->cost = cost;
-        list = vector(cost.size()+1, -1);
-        list[0] = 0;
-        list[1] = 0;
+        memo = vector(cost.size()+1, -1);
+        memo[0] = 0;
+        memo[1] = 0;
         return dp(cost.size());
     }
     int dp(int idx)
     {
-        for(int i = 2; i <= cost.size(); i++)
+        if(memo[idx] != -1)
         {
-            if(list[i] >= 0)
-            {
-                return list[i];
-            }
-            list[i] = min(list[i-2]+ cost[i-2], list[i-1] + cost[i-1]);
+            return memo[idx];
         }
-        return list[idx];
+        memo[idx] = min( dp(idx-1) + cost[idx-1], dp(idx-2) + cost[idx-2]);
+        return memo[idx];
     }
 };
