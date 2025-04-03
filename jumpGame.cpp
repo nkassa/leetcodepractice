095dd1c93cbc87f6d855aa119185aa812c7ebf78
natan kassa <nkassa@usc.edu>
2025-04-03 00:13:44 -0700

    new problem

--- jumpGame.cpp
+++ jumpGame.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
-
-    vector<int> memo;
-    int n;
-    bool ans = true;
-    vector<int> nums;
     bool canJump(vector<int>& nums) 
     {
-        this->nums = nums;
-        n = nums.size();
+        int n = nums.size();
+        int goal = n-1;
         if(n == 1)
         {
             return true;
         }
-        memo = vector(n, -1);
-        memo[0] = nums[0];
-        int result = dp(n-2);
-        if(ans == false)
+        for(int i = n-2; i >= 0; i--)
         {
-            return false;
+            if(i + nums[i] >= goal)
+            {
+                goal = i;
+            }
+            else
+            {
+                return false;
+            }
         }
-        return result >= n-1;
-    }
-    int dp(int idx)
-    {
-        if(memo[idx] != -1)
-        {
-            return memo[idx];
-        }
-        memo[idx] = max(dp(idx-1), idx + nums[idx]);
-        if(memo[idx-1] < idx)
-        {
-            ans = false;
-        }
-        return memo[idx];
+        return true;
     }
 };
