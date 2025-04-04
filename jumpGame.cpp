2af5bc859df7f92fe08ab600fa8051a8aef4771d
natan kassa <nkassa@usc.edu>
2025-04-04 01:06:42 -0700

    new problem

--- jumpGame.cpp
+++ jumpGame.cpp
@@ -7,9 +7,13 @@ public:
     {
         this->nums = nums;
         n = nums.size();
+        if(n == 1)
+        {
+            return true;
+        }
         memo = vector(n+1, -1);
         memo[0] = 0;
-        return dp(n-1) == n-1;
+        return dp(n-1) >= n-1;
     }
     int dp(int idx)
     {
@@ -18,6 +22,7 @@ public:
             return memo[idx];
         }
         memo[idx] = max(dp(idx-1), idx-1 + nums[idx-1]);
+        cout << memo[idx] << endl;
         return memo[idx];
     }
 };
