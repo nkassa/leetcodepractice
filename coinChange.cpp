b75bd432d0e60a1180c8785401b74e45822cd9d8
natan kassa <nkassa@usc.edu>
2025-04-11 22:18:03 -0700

    new problem

--- coinChange.cpp
+++ coinChange.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
-    vector<int> memo;
-    vector<int> coins;
-    int n;
     int coinChange(vector<int>& coins, int amount) 
     {
-        this->coins = coins;
-        sort(coins.begin(), coins.end());
-        n = amount + 1;
-        memo = vector(n, n);
+        int n = amount+1;
+        vector<int> memo(n,n);
         memo[0] = 0;
-        int result = dp(amount);
-        if(result > amount)
+        for(int i = 1; i <= amount; i++)
         {
-            return -1;
-        }
-        return result;
-    }
-    int dp(int idx)
-    {
-        if(memo[idx] != n)
-        {
-            return memo[idx];
-        }
-        for(int i = 0; i < coins.size(); i++)
-        {
-            if(idx - coins[i] >= 0)
+            for(int j = 0; j < coins.size(); j++)
             {
-                memo[idx] = min(memo[idx], dp(idx-coins[i]) + 1);
-            }
-            else
-            {
-                break;
+                if(i - coins[j] >= 0)
+                {
+                    memo[i] = min(memo[i], 1 + memo[i-coins[j]]);
+                }
             }
         }
-        return memo[idx];
+        if(memo[amount] != n)
+        {
+            return memo[amount];
+        }
+        return -1;
     }
 };
