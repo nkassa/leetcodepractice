64c7f49ee5703acd31774ab932885e881b6b1f2c
natan kassa <nkassa@usc.edu>
2025-04-12 00:24:18 -0700

    new problem

--- nthTribonacci.cpp
+++ nthTribonacci.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    vector<int> memo;
     int tribonacci(int n) 
     {
         if(n == 0)
         {
             return 0;
         }
-        if(n == 1 || n== 2)
+        else if(n == 1 || n == 2)
         {
             return 1;
         }
-        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+        vector<int> memo(n+1, -1);
+        memo[0]= 0;
+        memo[1] = 1;
+        memo[2] = 1;
+        for(int i = 3; i <= n; i++)
+        {
+            memo[i] = memo[i-1] + memo[i-2] + memo[i-3];
+        }
+        return memo[n];
     }
 };
