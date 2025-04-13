c18026838ea7d6ba62a5e7a549acd515b9c774f0
natan kassa <nkassa@usc.edu>
2025-04-13 01:56:05 -0700

    new problem

--- pascalsTriangle.cpp
+++ pascalsTriangle.cpp
@@ -2,29 +2,15 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) 
     {
-        vector<vector<int>> ans(numRows);
-        if(numRows == 1)
+        vector<vector<int>> ans;
+        for(int i = 0; i < numRows; i++)
         {
-            ans[0] = {1};
-            return ans;
-        }
-        else if(numRows == 2)
-        {
-            ans[0] = {1};
-            ans[1] = {1,1};
-            return ans;
-        }
-        ans[0] = {1};
-        ans[1] = {1,1};
-        for(int i = 2; i < numRows; i++)
-        {
-            vector<int> curr = {1};
-            for(int j = 0; j < i-1; j++)
+            vector<int> row(i+1, 1);
+            for(int j = 1; j < i; j++)
             {
-                curr.push_back(ans[i-1][j]+ans[i-1][j+1]);
+                row[j] = ans[i-1][j-1] + ans[i-1][j];
             }
-            curr.push_back(1);
-            ans[i] = curr;
+            ans.push_back(row);
         }
         return ans;
     }
