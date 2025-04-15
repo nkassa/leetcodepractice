90b47a7af33295d7d2f0fb7ceca44716ef2d3f56
natan kassa <nkassa@usc.edu>
2025-04-14 23:36:03 -0700

    new problem

--- targetSum.cpp
+++ targetSum.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
-    vector<char> opt = {'-', '+'};
     int findTargetSumWays(vector<int>& nums, int target) 
     {
         int curr = 0;
         int sol = 0;
-        backtracking(curr, sol, nums, target, 0);
+        dp(curr, sol, nums, target, 0);
         return sol;
     }
-    void backtracking(int curr, int& sol, vector<int>& nums, int& target, int size)
+    void dp(int curr, int& sol, vector<int>& nums, int& target, int idx)
     {
-        if(size == nums.size() && curr == target)
+        if(idx == nums.size())
         {
-            sol++;
+            if(curr == target)
+            {
+                sol++;
+            }
             return;
         }
-        for(int i = size; i < nums.size(); i++)
-        {
-            for(int j = 0; j < opt.size(); j++)
-            {
-                if(opt[j] == '-')
-                {
-                    curr -= nums[i];
-                    backtracking(curr, sol, nums, target, size+1);
-                    curr += nums[i];
-                }
-                else
-                {
-                    curr += nums[i];
-                    backtracking(curr, sol, nums, target, size+1);
-                    curr -= nums[i];
-                }
-            }
-        }
+        dp(curr - nums[idx], sol, nums, target, idx + 1);
+        dp(curr + nums[idx], sol, nums, target, idx + 1);
     }
 };
