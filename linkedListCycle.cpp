69d69d1927e63c2fd8c42e44a8f6bb3febcf1551
natan kassa <nkassa@usc.edu>
2025-04-15 21:10:49 -0700

    new problem

--- linkedListCycle.cpp
+++ linkedListCycle.cpp
@@ -10,13 +10,13 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) 
     {
-        ListNode* slow = head;
         ListNode* fast = head;
+        ListNode* slow = head;
         while(fast != nullptr && fast->next != nullptr)
         {
-            slow = slow->next;
             fast = fast->next->next;
-            if(slow == fast)
+            slow = slow->next;
+            if(fast == slow)
             {
                 return true;
             }
