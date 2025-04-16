02271707a49b8587b6ec4e26184064d63a67d3bd
natan kassa <nkassa@usc.edu>
2025-04-16 00:25:54 -0700

    new problem

---
+++ copyListWithRandomPointer.cpp
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) 
+    {
+        Node* ans = new Node(0);
+        Node* curr = ans;
+        curr->next = nullptr;
+        curr = curr->next;
+        while(head != nullptr)
+        {
+            curr = new Node(head->val);
+            curr->random = head->random;
+            head = head->next;
+            curr = curr->next;
+        }
+        return ans;
+    }
+};
