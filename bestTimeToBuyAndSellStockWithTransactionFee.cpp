3331168ddf6f4d18027308cb4fecd452f805ab36
natan kassa <nkassa@usc.edu>
2025-04-17 18:39:09 -0700

    new problem

---
+++ bestTimeToBuyAndSellStockWithTransactionFee.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> prices;
+    vector<vector<int,int>> memo;
+    int fee;
+    int maxProfit(vector<int>& prices, int fee) 
+    {
+        this->prices = prices;
+        this->fee = fee;
+        memo = vector(prices.size(), vector(2, 0));
+        return dp(0, 0);
+    }
+    int dp(int i, int holding)
+    {
+        if(i == prices.size())
+        {
+            return 0;
+        }
+        int ans = dp(i+1, holding);
+        if(holding == 1)
+        {
+            ans = max(ans, prices[i] + dp(i+1, 0) - fee);
+        }
+        else
+        {
+            ans = max(ans, dp(i+1, 1) - prices[i])
+        }
+        memo[i][holding] = ans;
+        return memo[i][holding];
+    }
+};
