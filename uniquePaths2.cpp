77982a89361e52d6acfd063f588b7840e5c38bef
natan kassa <nkassa@usc.edu>
2025-04-19 16:26:13 -0700

    new problem

--- uniquePaths2.cpp
+++ uniquePaths2.cpp
@@ -4,8 +4,13 @@ public:
     vector<vector<int>> memo;
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
     {
+        if(obstacleGrid[0][0] == 1)
+        {
+            return 0;
+        }
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
+        this->obstacleGrid = obstacleGrid;
         memo = vector(m, vector<int>(n, -1));
         memo[0][0] = 1;
         return dp(m-1, n-1);
