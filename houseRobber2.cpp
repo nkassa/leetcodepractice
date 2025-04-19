266fbe80b9b068f07ff1b0d0c23556ebdd53df0c
natan kassa <nkassa@usc.edu>
2025-04-18 23:44:21 -0700

    new problem

--- houseRobber2.cpp
+++ houseRobber2.cpp
@@ -4,32 +4,21 @@ public:
     vector<int> memo;
     int rob(vector<int>& nums) 
     {
-        this->nums = nums;
         int n = nums.size();
         if(n == 1)
         {
             return nums[0];
         }
+        if(n == 2)
+        {
+            return 0;
+        }
         memo = vector(n, -1);
-        if(nums[0] > nums[n-1])
-        {
-            nums[n-1] = 0;
-        }
-        else
-        {
-            nums[0] = 0
-        }
         memo[0] = nums[0];
-        memo[1] = max(nums[0], nums[1]);
-        return dp(n-1);
-    }
-    int dp(int idx)
-    {
-        if(memo[idx] != -1)
+        memo[1] = max(nums[])
+        for(int i = 0; i < n -1; i++)
         {
-            return memo[idx];
+            memo[2] = 
         }
-        memo[idx] = max(dp(idx-2)+ nums[idx], dp(idx-1));
-        return memo[idx];
     }
 };
