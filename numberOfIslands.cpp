f737cf581e1078a30c59dd6d38e3d0ac5bf3cf4d
natan kassa <nkassa@usc.edu>
2025-04-21 13:03:00 -0700

    new problem

--- numberOfIslands.cpp
+++ numberOfIslands.cpp
@@ -2,45 +2,49 @@ class Solution {
 public:
     int m;
     int n;
-    vector<vector<bool>> seen;
     vector<vector<int>> directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+    vector<vector<char>> grid;
+    vector<vector<bool>> seen;
     int numIslands(vector<vector<char>>& grid) 
     {
-        int ans= 0;
         m = grid.size();
         n = grid[0].size();
+        this->grid = grid;
         seen = vector(m, vector<bool>(n, false));
+        queue<pair<int, int>> queue;
+        int ans = 0;
         for(int row = 0; row < m; row++)
         {
             for(int col = 0; col < n; col++)
             {
-                if(grid[row][col] == '1' && seen[row][col] == false)
+                if(!seen[row][col] && grid[row][col] == '1')
                 {
                     ans++;
+                    queue.push({row,col});
                     seen[row][col] = true;
-                    dfs(row, col, grid);
+                    while(!queue.empty())
+                    {
+                        int Row = queue.front().first;
+                        int Col = queue.front().second;
+                        queue.pop();
+                        for(vector<int> direction: directions)
+                        {
+                            int nextRow = direction[0] + Row;
+                            int nextCol = direction[1] + Col;
+                            if(valid(nextRow, nextCol) && !seen[nextRow][nextCol])
+                            {
+                                queue.push({nextRow, nextCol});
+                                seen[nextRow][nextCol] = true;
+                            }
+                        }
+                    }
                 }
             }
         }
         return ans;
     }
-    void dfs(int row, int col, vector<vector<char>>& grid)
+    bool valid(int row, int col)
     {
-        for(vector<int> direction: directions)
-        {
-            int nextRow = row + direction[0];
-            int nextCol = col + direction[1];
-            if(valid(nextRow, nextCol, grid) && seen[nextRow][nextCol] == false)
-            {
-                seen[nextRow][nextCol] = true;
-                dfs(nextRow,nextCol, grid);
-            }
-        }
+        return row >= 0 && row < m && col >= 0 && col < n && grid[row][col] == '1';
     }
-
-    bool valid(int row, int col, vector<vector<char>>& grid)
-    {
-        return 0 <= row && row < m && 0 <= col && col < n && grid[row][col] == '1';
-    }
-
 };
