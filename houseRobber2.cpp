c547eabe08c2d2054db56a76e12195c0d4c26d28
natan kassa <nkassa@usc.edu>
2025-04-20 20:44:54 -0700

    new problem

--- houseRobber2.cpp
+++ houseRobber2.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
+    int n;
     int rob(vector<int>& nums) 
     {
         int ans = 0;
-        int n = nums.size();
-        vector<int> memo;
+        n = nums.size();
         if(n == 1)
         {
             return nums[0];
@@ -13,32 +13,19 @@ public:
         {
             return max(nums[0], nums[1]);
         }
-
-        vector<int> numsTwo;
-        for(int i = 1; i < nums.size(); i++)
-        {
-            numsTwo.push_back(nums[i]);
-        }
-
-        memo = vector(nums.size(), 0);
-        memo[0] = nums[0];
-        memo[1] = max(nums[0], nums[1]);
-
-        for(int i = 2; i < nums.size()-1; i++)
-        {
-            memo[i] = max(memo[i-2]+ nums[i], memo[i-1]);
-        }
-        ans = memo[nums.size()-2];
-
-        memo = vector(numsTwo.size(), 0);
-        memo[0] = numsTwo[0];
-        memo[1] = max(numsTwo[0], numsTwo[1]);
-
-        for(int i = 2; i < numsTwo.size(); i++)
-        {
-            memo[i] = max(memo[i-2]+ numsTwo[i], memo[i-1]);
-        }
-        ans = max(ans, memo[numsTwo.size()-1]);
+        ans = dp(0, n-1, nums);
+        ans = max(ans, dp(1, n, nums));
         return ans;
+    }
+    int dp(int start, int end, vector<int>& nums)
+    {
+        vector<int> memo(end, 0);
+        memo[start] = nums[start];
+        memo[start+1] = max(nums[start], nums[start+1]);
+        for(int i = start+2; i < end; i++)
+        {
+            memo[i] = max(memo[i-1], memo[i-2] + nums[i]);
+        }
+        return memo[end-1];
     }
 };
