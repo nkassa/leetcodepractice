bd39a05613b6bda7f56cd51715e87a1aeb7e4530
natan kassa <nkassa@usc.edu>
2025-04-22 11:23:05 -0700

    new problem

---
+++ kthLargestElementInAnArray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        priority_queue<int> heap;
+        for(int num: nums)
+        {
+            heap.push(num);
+        }
+        for(int i = 0; i < k-1; i++)
+        {
+            heap.pop();
+        }
+        return heap.top();
+    }
+};
