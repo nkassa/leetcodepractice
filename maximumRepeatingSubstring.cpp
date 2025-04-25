3291e0761392f472d267788325bf1aa46a3b3725
natan kassa <nkassa@usc.edu>
2025-04-24 23:24:35 -0700

    new problem

--- maximumRepeatingSubstring.cpp
+++ maximumRepeatingSubstring.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     string sequence;
     string word;
-    int ans = 0;
+    vector<int> memo;
     int maxRepeating(string sequence, string word) 
     {
         this->sequence = sequence;
         this->word = word;
+        memo = vector(sequence.size(), 0);
         return dp(0, 0);
     }
     int dp(int seqIdx, int wordIdx)
     {
         if(seqIdx == sequence.size())
         {
-            return ans;
+            return 0;
         }
         if(wordIdx == word.size())
         {
             return 1;
         }
+        int ans = 0;
         if(sequence[seqIdx] == word[wordIdx])
         {
             ans += dp(seqIdx + 1, wordIdx + 1);
@@ -27,6 +29,7 @@ public:
         {
             ans +=  dp(seqIdx + 1, 0);
         }
-        return ans;
+        memo[seqIdx] = ans;
+        return memo[seqIdx];
     }
 };
