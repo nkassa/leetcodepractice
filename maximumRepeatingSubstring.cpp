608da56bd368bc60bb44f41d0274105cb9839033
natan kassa <nkassa@usc.edu>
2025-04-24 20:28:32 -0700

    new problem

--- maximumRepeatingSubstring.cpp
+++ maximumRepeatingSubstring.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
+    string sequence;
+    string word;
+    int ans = 0;
     int maxRepeating(string sequence, string word) 
     {
-        int ans = 0;
-        int seqIdx = 0;
-        int wordIdx = 0;
-        while(seqIdx != sequence.size())
+        this->sequence = sequence;
+        this->word = word;
+        return dp(0, 0);
+    }
+    int dp(int seqIdx, int wordIdx)
+    {
+        if(seqIdx == sequence.size())
         {
-            cout << ans << " " << seqIdx << " " << wordIdx << endl;
-            if(sequence[seqIdx] == word[wordIdx])
-            {
-                wordIdx++;
-            }
-            else
-            {
-                wordIdx = 0;
-            }
-            if(wordIdx == word.size())
-            {
-                ans++;
-                wordIdx = 0;
-            }
-            seqIdx++;
+            return ans;
+        }
+        if(wordIdx == word.size())
+        {
+            return 1;
+        }
+        if(sequence[seqIdx] == word[wordIdx])
+        {
+            ans += dp(seqIdx + 1, wordIdx + 1);
+        }
+        else
+        {
+            ans +=  dp(seqIdx + 1, 0);
         }
         return ans;
     }
