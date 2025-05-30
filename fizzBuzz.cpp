a6db087d774e00d5ad67e4acf6f2e1852cbc7a17
natan kassa <nkassa@usc.edu>
2025-05-30 13:41:09 -0700

    Add problem

--- fizzBuzz.cpp
+++ fizzBuzz.cpp
@@ -3,24 +3,23 @@ public:
     vector<string> fizzBuzz(int n) 
     {
         vector<string> ans(n, "");
-        for(int idx = 0; idx < n; idx++)
+        for(int num = 1; num <= n; num++)
         {
-            int num = idx+1;
             if(num % 3 == 0 && num % 5 == 0)
             {
-                ans[idx] = "FizzBuzz";
+                ans[num-1] = "FizzBuzz";
             }
             else if(num % 3 == 0)
             {
-                ans[idx] = "Fizz";
+                ans[num-1] = "Fizz";
             }
             else if(num % 5 == 0)
             {
-                ans[idx] = "Buzz";
+                ans[num-1] = "Buzz";
             }
             else
             {
-                ans[idx]= to_string(num);
+                ans[num-1]= to_string(num);
             }
         }
         return ans;
