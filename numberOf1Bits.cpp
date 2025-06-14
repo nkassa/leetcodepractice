f96084b5ba7e5d296639418b16db624bfb540405
natan kassa <nkassa@usc.edu>
2025-06-14 12:59:42 -0700

    Add problem

--- numberOf1Bits.cpp
+++ numberOf1Bits.cpp
@@ -2,12 +2,6 @@ class Solution {
 public:
     int hammingWeight(int n) 
     {
-        int cnt = 0;
-        while(n != 0)
-        {
-            cnt += n % 2;
-            n /= 2;
-        }
-        return cnt;
+        return __builtin_popcount(n);
     }
 };
