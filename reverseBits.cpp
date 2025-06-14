396ca1d5203e62b12afd97c1fea6f97955019eb7
natan kassa <nkassa@usc.edu>
2025-06-14 13:42:13 -0700

    Add problem

--- reverseBits.cpp
+++ reverseBits.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) 
     {
-        string s = bitset<32>(n).to_string();
+        bitset<32> b(n);
+        string s = b.to_string();
         reverse(s.begin(), s.end());
         
         uint32_t ans = 0;
