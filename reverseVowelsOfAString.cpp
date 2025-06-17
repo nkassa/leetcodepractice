62781ad0aa9d1e68cdda67bf034124c1a67eecc9
natan kassa <nkassa@usc.edu>
2025-06-16 23:35:16 -0700

    Add problem

--- reverseVowelsOfAString.cpp
+++ reverseVowelsOfAString.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    unordered_set<char> vowel = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
     string reverseVowels(string s) 
     {
+        unordered_set<char> seen = {'a','e','i','o','u','A','E','I','O','U'};
         int left = 0;
         int right = s.size()-1;
         while(left < right)
         {
-            if(vowel.find(s[left]) != vowel.end() && vowel.find(s[right]) != vowel.end())
+            if(seen.find(s[left]) == seen.end())
+            {
+                left++;
+            }
+            if(seen.find(s[right]) == seen.end())
+            {
+                right--;
+            }
+            if(seen.find(s[left]) != seen.end() && seen.find(s[right]) != seen.end())
             {
                 char temp = s[left];
                 s[left] = s[right];
                 s[right] = temp;
-                left++;
-                right--;
-            }
-            else if(vowel.find(s[left]) != vowel.end())
-            {
-                right--;
-            }
-            else if(vowel.find(s[right]) != vowel.end())
-            {
-                left++;
-            }
-            else
-            {
                 left++;
                 right--;
             }
