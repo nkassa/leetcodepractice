ecb5c5cd9a5962c23c07296e8d225ab7a542729b
natan kassa <nkassa@usc.edu>
2025-06-30 01:37:56 -0700

    Add problem

--- fruitIntoBaskets.cpp
+++ fruitIntoBaskets.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         unordered_map<int,int> count;
-        for(int fruit: fruits)
-        {
-            count[fruit]++;
-        }
-        priority_queue<int, vector<int>, greater<int>> heap;
-        for(auto [key, value]: count)
-        {
-            heap.push(value);
-        }
+        int left = 0;
         int ans = 0;
-        int k = 0;
-        while(k < 2 && !heap.empty())
+        for(int right = 0; right < fruits.size(); right++)
         {
-            k++;
-            ans += heap.top();
-            heap.pop();
+            while(count.size() > 2)
+            {
+                count[fruits[left]]--;
+                if(count[fruits[left]] == 0)
+                {
+                    count.erase(fruits[left]);
+                }
+                left++;
+            }
+            count[fruits[right]]++;
+            ans = max(ans, right-left+1);
         }
         return ans;
-    }
+    }   
 };
